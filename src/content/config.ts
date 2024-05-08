16fbf9f600629b8efde69790146b651be905cf87
Nicolas A. Vera <nicolasalevera98@gmail.com>
2024-05-08 12:03:50 -0300

    Add MoreWork section and refactor styles

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,20 @@
 import { defineCollection, z } from "astro:content";
 
-const featureProjectsCollection = defineCollection({
+const projectsCollection = defineCollection({
   type: "data",
   schema: z.object({
     name: z.string(),
     logoIcon: z.optional(z.string()),
     about: z.string(),
+    description: z.string(),
     repository: z.optional(z.string()),
     demo: z.optional(z.string()),
+    featured: z.optional(z.boolean()),
     screenshots: z.array(z.string()),
     techs: z.array(z.string()),
   }),
 });
 
 export const collections = {
-  "featured-projects": featureProjectsCollection,
+  projects: projectsCollection,
 };
