95d89b028a44d7b58de1dc4640aa547d7e803792
Nicolas A. Vera <nicolasalevera98@gmail.com>
2024-05-09 23:33:21 -0300

    Improve AboutMe animations

--- src/scripts/configGsap.ts
+++ src/scripts/configGsap.ts
@@ -1,4 +1,6 @@
 import { gsap } from "gsap";
+import { SlowMo } from "gsap/EasePack";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(SlowMo);
