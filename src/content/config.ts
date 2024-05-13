a7aa6b5ace7818d143c03867d26098d21c7ff3eb
Nicolas A. Vera <nicolasalevera98@gmail.com>
2024-05-13 17:07:15 -0300

    Update Work section

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,17 +2,18 @@ import { defineCollection, z } from "astro:content";
 
 const projectsCollection = defineCollection({
   type: "data",
-  schema: z.object({
-    name: z.string(),
-    logoIcon: z.optional(z.string()),
-    about: z.string(),
-    description: z.string(),
-    repository: z.optional(z.string()),
-    demo: z.optional(z.string()),
-    featured: z.optional(z.boolean()),
-    screenshots: z.array(z.string()),
-    techs: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      logoIcon: z.optional(z.string()),
+      about: z.string(),
+      description: z.string(),
+      repository: z.optional(z.string()),
+      demo: z.optional(z.string()),
+      featured: z.optional(z.boolean()),
+      screenshots: z.array(image()),
+      techs: z.array(z.string()),
+    }),
 });
 
 export const collections = {
