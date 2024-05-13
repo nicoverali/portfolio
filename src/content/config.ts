9c52d5ed092ea79442da62792e8dd7b084792c37
Nicolas A. Vera <nicolasalevera98@gmail.com>
2024-05-13 19:27:35 -0300

    Update OtherWork/MoreProjects section

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const projectsCollection = defineCollection({
     z.object({
       name: z.string(),
       logoIcon: z.optional(z.string()),
-      about: z.string(),
+      short: z.string(),
       description: z.string(),
       repository: z.optional(z.string()),
       demo: z.optional(z.string()),
