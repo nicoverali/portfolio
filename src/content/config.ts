3f4fa4d115ea3706cda09e0cc6225d93266dff99
Nicolas A. Vera <nicolasalevera98@gmail.com>
2024-05-31 11:33:09 -0300

    Add redesigned Work section

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,12 +5,11 @@ const projectsCollection = defineCollection({
   schema: ({ image }) =>
     z.object({
       name: z.string(),
-      logoIcon: z.optional(z.string()),
       short: z.string(),
       description: z.string(),
       repository: z.optional(z.string()),
       demo: z.optional(z.string()),
-      featured: z.optional(z.boolean()),
+      featuredImage: image(),
       screenshots: z.array(image()),
       techs: z.array(z.string()),
     }),
