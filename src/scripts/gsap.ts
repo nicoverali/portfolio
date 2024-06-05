4e7385ac521f9202df0d9d9bd026735bc01b8cf7
Nicolas A. Vera <nicolasalevera98@gmail.com>
2024-06-05 18:46:11 -0300

    Add interactions to main skills graphic

--- src/scripts/gsap.ts
+++ src/scripts/gsap.ts
@@ -1,8 +1,10 @@
 import { gsap } from "gsap";
 import { SlowMo } from "gsap/EasePack";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Draggable } from "gsap/Draggable";
 
 export const setupGSAP = () => {
   gsap.registerPlugin(ScrollTrigger);
   gsap.registerPlugin(SlowMo);
+  gsap.registerPlugin(Draggable);
 };
--- src/scripts/utils/gsap.ts
+++ src/scripts/utils/gsap.ts
@@ -1,3 +1,9 @@
+import { gsap } from "gsap";
+
 export const vhPercent = (percent: number): number => {
   return window.innerHeight * (percent / 100);
+};
+
+export const getRotationOf = (element: HTMLElement): number => {
+  return gsap.getProperty(element, "rotation") as number;
 };
